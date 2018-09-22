1d3505c739eda3eb91516ac8b5bf605f4d9c9ab4
NiickCP <36265129+NiickCP@users.noreply.github.com>
2018-09-22 12:49:51 -0300

    Add files via upload

--- persona2.py
+++ persona2.py
@@ -10,14 +10,18 @@ class Personaje:
 		
 	def recibir_ataque(self,ataque):
 		self.__salud-=ataque
+
 	def __generar_ataque(self):
 		return randint(0,self.__ataque)
+
 	def atacar(self,enemigo):
 		ataque=self.__generar_ataque()
 		enemigo.recibir_ataque(ataque)
+		
 	@property		""decorator""
 	def salud(self):
 		return self.__salud
+
 	def __str__(self)""Para poder ver mas que la direccion de donde esta el personaje""
 		plantilla="{nombre} salud={salud} ataque={ataque}"
 		return plantilla.format(
